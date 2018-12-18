dfa3c5f8359d93395d1eb0ff113a9602ea05b87a
Tobe28 <tobiasbernasconi@hotmail.ch>
2018-12-18 22:01:31 +0100

    Testing Profile stuff

--- my-profile/src/app/app.component.ts
+++ my-profile/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-profile';
+  title = 'Tobe';
+  fullname = 'Tobias Bernasconi';
+  birthdate = '03.07.1998';
+  country = 'Switzerland';
+  lang = 'German, English, Finish, Italian';
+  programming = "C#, Java, JavaScript, TypeScript, Angular";
+
+  games = 'CS:GO, Tomb Raider';
+  sports = 'Skiing, Eishockey';
+  esportTeams = 'Fnatic, Team Liquid, SK Telecom';
+  sportTeams = 'Winninpeg Jets';
 }
